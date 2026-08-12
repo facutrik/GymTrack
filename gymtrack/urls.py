d87814fc9aaf768c3b6137a3fcd9d1bd7fcc2245
facutrik <facultrik@gmail.com>
2026-08-11 21:58:51 -0300

    Agrego nuevas cosas

--- gymtrack/urls.py
+++ gymtrack/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     # Redirige la raíz ('/') directamente al login
@@ -26,4 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
