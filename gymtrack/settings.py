2fbb52ef4c11ae0b0c9b8aafc560e02825bb1ca6
facutrik <facultrik@gmail.com>
2026-08-14 17:17:42 -0300

    Cambios

--- gymtrack/settings.py
+++ gymtrack/settings.py
@@ -117,5 +117,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 #config por defecto de claves primarias
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = 'login'
